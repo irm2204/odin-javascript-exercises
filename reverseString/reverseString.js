6008054acbcd7c4c51588112b85fae861d9171aa
Ian Mason <irm2204@gmail.com>
2018-03-02 11:34:59 +0000

    Completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-var reverseString = function() {
-
+var reverseString = function(inString) {
+  let outString = "";
+  let i = inString.length - 1;
+  while (i >= 0) {
+    outString = outString + inString.charAt(i);
+    i--;
+  }
+  return outString;
 }
 
 module.exports = reverseString
