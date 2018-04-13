86ea11a37a79ecc9e75237dd15787cecab96f55b
Ian Mason <irm2204@gmail.com>
2018-04-13 10:33:23 +0100

    removeFromArray completed

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-var removeFromArray = function() {
-
+var removeFromArray = function(inputArray) {
+  //This function removes any values provided as arguments from inputArray
+  //Loop through all arguments provided, excluding the first where the array is. This is why we start at 1
+  for (i = 1; i < arguments.length; i++) {
+    //Loop through the array, checking to see if the argument exists, then removing it
+    let j = 0;
+    while (j < inputArray.length) {
+    //Compare the argument to the array element
+      if (arguments[i] === inputArray[j]) { 
+      // Remove match, don't increment j
+        inputArray.splice (j,1)
+      }
+      else { 
+      //Move on to the next one
+        j++;
+      }
+    }
+  }
+  return inputArray
 }
 
 module.exports = removeFromArray
